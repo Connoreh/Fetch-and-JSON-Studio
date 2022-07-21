00f3b11a3a97a34c495c25cd2b1490e14886b985
Connor Hellman <108197430+Connoreh@users.noreply.github.com>
2022-07-21 17:13:49 -0500

    Finished Studio minus bonus

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 // TODO: add code here
+window.addEventListener("load", function () { 
+    this.fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+response.json().then( function(json) { 
+const astronautList = document.getElementById("container");
+for(i=0; i <= json.length; i++) {
+    astronautList.innerHTML += `
+    <div class="astronaut">
+    <div class="bio">
+    <h3>${json[i].firstName}  ${json[i].lastName}</h3>
+    <ul> 
+    <li>Skills: ${json[i].skills}</li>
+    <li>Hours in Space: ${json[i].hoursInSpace}</li>
+    <li>active: ${json[i].active}</li>
+    </ul>
+    </div>
+    <img class="avatar" src="${json[i].picture}">
+    </div> 
+
+`
+if(json[i].active === true) {
+json.active
+}
+
+
+}
+});
+    
+    });
+});
